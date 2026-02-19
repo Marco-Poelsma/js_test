dfb1ff6958c5f8f9616b85498b81bea5151e39a9
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2026-02-19 12:04:20 +0100

    feat(/scripts/main.js): declaring varaibles

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
 console.log("Hello World!");
 
 const hola = document.getElementById("hola");
-console.log(hola)
+console.log(hola);
+
+//declaring variables
+var a = 42; //deprecated
+a = "ahora eres un string";
+
+let b = 42; //modern declaration of variables
+b = "ahora eres un string";
+
+const c = 42; //constant (invariable)
+

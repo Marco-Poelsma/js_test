37d9ce6ed1e46bc0cd0eb68953c1ff37e45a2426
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2026-02-19 12:13:38 +0100

    feat(/scripts/main.js): variable types

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ console.log("Hello World!");
 const hola = document.getElementById("hola");
 console.log(hola);
 
-//declaring variables
+// MARK: DECLARING VARIABLES
 var a = 42; //deprecated
 a = "ahora eres un string";
 
@@ -12,3 +12,21 @@ b = "ahora eres un string";
 
 const c = 42; //constant (invariable)
 
+// MARK: VARIABLE TYPES
+// NUMBER
+const n1 = 100; // Numbers can be integers
+const n2 = 4.6; // Or floats
+
+// STRING
+const s1 = "A string";
+const s2 = 'A string';
+const s3 = `A string`; // This one also lets us add stuff like newline characters, as well as other useful things
+
+// BOOLEAN
+const b1 = true; // Lowercase
+const b2 = false;
+
+// ARRAYS
+const arr1 = []; // Empty Array
+const arr2 = [1, 2, 3, 4, 5, 6, 7];
+const arr3 = [1, 2, "hola", true];

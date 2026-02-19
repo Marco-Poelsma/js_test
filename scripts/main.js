ab743940aca6d130803b603473c953841b495784
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2026-02-19 12:24:25 +0100

    feat(/scripts/main.js): arrays, objects, null, and undefined

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,32 @@ const b1 = true; // Lowercase
 const b2 = false;
 
 // ARRAYS
+/**
+ * Since arrays are dealt with pointers, it is important to declare them as const to avoid overwriting them later on.
+ */
 const arr1 = []; // Empty Array
 const arr2 = [1, 2, 3, 4, 5, 6, 7];
 const arr3 = [1, 2, "hola", true];
+
+// OBJECTS
+const obj1 = {
+    name: "Test",
+    surname: "Yes",
+    pokemon: "Umbreon",
+    age: 23094823,
+    hobbies: ["A", "B", "C"]
+};
+
+
+// ACCESSING VARIABLES INSIDE ARRAYS OR OBJECTS
+console.log(arr2[4]);
+console.log(obj1.surname);
+console.log(obj1["surname"]);
+console.log(obj1.hobbies);
+
+// NULL
+console.log(null);
+
+// UNDEFINED
+let ant;
+console.log(ant);
